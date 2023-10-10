e7a08e56ed922e63d11c2eea83c890afcb1262c3
yunes moahammadi <younesm837@gmail.com>
2023-10-10 13:46:10 +0330

    add spa link mode in project

--- src/component/sideBar/sideBar.jsx
+++ src/component/sideBar/sideBar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import "./sideBar.css"
@@ -22,24 +23,24 @@ const options = [
     scroll: false,
     backdrop: true,
   },
-//   {
-//     name: 'Disable backdrop',
-//     scroll: false,
-//     backdrop: false,
-//   },
-//   {
-//     name: 'Enable body scrolling',
-//     scroll: true,
-//     backdrop: false,
-//   },
-//   {
-//     name: 'Enable both scrolling & backdrop',
-//     scroll: true,
-//     backdrop: true,
-//   },
+  //   {
+  //     name: 'Disable backdrop',
+  //     scroll: false,
+  //     backdrop: false,
+  //   },
+  //   {
+  //     name: 'Enable body scrolling',
+  //     scroll: true,
+  //     backdrop: false,
+  //   },
+  //   {
+  //     name: 'Enable both scrolling & backdrop',
+  //     scroll: true,
+  //     backdrop: true,
+  //   },
 ];
 
- function OffCanvasExample({ name, scroll, backdrop, ...props }) {
+function OffCanvasExample({ name, scroll, backdrop, ...props }) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -60,86 +61,95 @@ const options = [
           <Offcanvas.Title>Dashboard</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-        <div className="sidebar">
-      <div className="sidebarWrapper">
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Dashboard</h3>
-          <ul className="sidebarList">
-            <li className="sidebarListItem active">
-              <LineStyleIcon className="sidebarIcon" />
-              Home
-            </li>
-            <li className="sidebarListItem">
-              <TimelineIcon className="sidebarIcon" />
-              Analytics
-            </li>
-            <li className="sidebarListItem">
-              <TrendingUpIcon className="sidebarIcon" />
-              Sales
-            </li>
-          </ul>
-        </div>
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Quick Menu</h3>
-          <ul className="sidebarList">
-            <li className="sidebarListItem">
-              <PermIdentityIcon className="sidebarIcon" />
-              Users
-            </li>
-            <li className="sidebarListItem">
-              <PermIdentityIcon className="sidebarIcon" />
-              New User
-            </li>
-            <li className="sidebarListItem">
-              <StorefrontIcon className="sidebarIcon" />
-              Products
-            </li>
-            <li className="sidebarListItem">
-              <AttachMoneyIcon className="sidebarIcon" />
-              Transactions
-            </li>
-            <li className="sidebarListItem">
-              <BarChartIcon className="sidebarIcon" />
-              Reports
-            </li>
-          </ul>
-        </div>
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Notifications</h3>
-          <ul className="sidebarList">
-            <li className="sidebarListItem">
-              <MailOutlineIcon className="sidebarIcon" />
-              Mail
-            </li>
-            <li className="sidebarListItem">
-              <DynamicFeedIcon className="sidebarIcon" />
-              Feedback
-            </li>
-            <li className="sidebarListItem">
-              <ChatBubbleOutlineIcon className="sidebarIcon" />
-              Messages
-            </li>
-          </ul>
-        </div>
-        <div className="sidebarMenu">
-          <h3 className="sidebarTitle">Staff</h3>
-          <ul className="sidebarList">
-            <li className="sidebarListItem">
-              <WorkOutlineIcon className="sidebarIcon" />
-              Manage
-            </li>
-            <li className="sidebarListItem">
-              <TimelineIcon className="sidebarIcon" />
-              Analytics
-            </li>
-            <li className="sidebarListItem">
-              <ReportIcon className="sidebarIcon" />
-              Reports
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+          <div className="sidebar">
+            <div className="sidebarWrapper">
+              <div className="sidebarMenu">
+                <h3 className="sidebarTitle">Dashboard</h3>
+                <ul className="sidebarList">
+                  <Link to="/" className='link'>
+                    <li className="sidebarListItem active">
+                      <LineStyleIcon className="sidebarIcon" />
+                      Home
+                    </li>
+                  </Link>
+                  <li className="sidebarListItem">
+                    <TimelineIcon className="sidebarIcon" />
+                    Analytics
+                  </li>
+                  <li className="sidebarListItem">
+                    <TrendingUpIcon className="sidebarIcon" />
+                    Sales
+                  </li>
+                </ul>
+              </div>
+              <div className="sidebarMenu">
+                <h3 className="sidebarTitle">Quick Menu</h3>
+                <ul className="sidebarList">
+                  <Link to="/users" className='link'>
+                    <li className="sidebarListItem">
+                      <PermIdentityIcon className="sidebarIcon" />
+                      Users
+                    </li>
+                  </Link>
+                  <Link to="/newuser" className='link'>
+                    <li className="sidebarListItem">
+                      <PermIdentityIcon className="sidebarIcon" />
+                      New User
+                    </li>
+                  </Link>
+
+                  <Link to="/product" className='link'>
+                  <li className="sidebarListItem">
+                    <StorefrontIcon className="sidebarIcon" />
+                    Products
+                  </li>
+                  </Link>
+                  <li className="sidebarListItem">
+                    <AttachMoneyIcon className="sidebarIcon" />
+                    Transactions
+                  </li>
+                  <li className="sidebarListItem">
+                    <BarChartIcon className="sidebarIcon" />
+                    Reports
+                  </li>
+                </ul>
+              </div>
+              <div className="sidebarMenu">
+                <h3 className="sidebarTitle">Notifications</h3>
+                <ul className="sidebarList">
+                  <li className="sidebarListItem">
+                    <MailOutlineIcon className="sidebarIcon" />
+                    Mail
+                  </li>
+                  <li className="sidebarListItem">
+                    <DynamicFeedIcon className="sidebarIcon" />
+                    Feedback
+                  </li>
+                  <li className="sidebarListItem">
+                    <ChatBubbleOutlineIcon className="sidebarIcon" />
+                    Messages
+                  </li>
+                </ul>
+              </div>
+              <div className="sidebarMenu">
+                <h3 className="sidebarTitle">Staff</h3>
+                <ul className="sidebarList">
+                  <li className="sidebarListItem">
+                    <WorkOutlineIcon className="sidebarIcon" />
+                    Manage
+                  </li>
+                  <li className="sidebarListItem">
+                    <TimelineIcon className="sidebarIcon" />
+                    Analytics
+                  </li>
+                  <li className="sidebarListItem">
+                    <ReportIcon className="sidebarIcon" />
+                    Reports
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </Offcanvas.Body>
       </Offcanvas>
     </>
