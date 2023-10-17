b709432aba29b3e3279102e40f158020139e8e03
yunes moahammadi <younesm837@gmail.com>
2023-10-17 23:37:58 +0330

    product part is completed

--- src/datas.js
+++ src/datas.js
@@ -154,4 +154,27 @@ let userRows = [
     }
 ]
 
-export { xAxisData, newMembers, transactions, userRows }
+
+let products = [
+    {
+        id: 1,
+        title: 'Asus',
+        avatar: 'images/asus.jpeg',
+        price: 890
+    },
+    {
+        id: 2,
+        title: 'Acer',
+        avatar: 'images/acer.jpg',
+        price: 890
+    },
+
+    {
+        id: 3,
+        title: 'Dell',
+        avatar: 'images/dell.jpg',
+        price: 890
+    },
+]
+
+export { xAxisData, newMembers, transactions, userRows, products }
